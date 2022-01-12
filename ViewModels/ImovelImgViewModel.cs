c36a52d9a9c109c17f46de0162d1b8e03edb14ed
OnikenX <onikenx@pm.me>
2022-01-12 14:50:59 +0000

    update

--- ViewModels/ImovelImgViewModel.cs
+++ ViewModels/ImovelImgViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Pweb_2021.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +10,18 @@ namespace Pweb_2021.ViewModels
 {
     public class ImovelImgViewModel
     {
+        public int ImovelImgId { get; set; }
 
+        public string Description { get; set; }
+
+        //caminho para a imagem
+        [Required(ErrorMessage = "Please choose profile image")]
+        [Display(Name = "Profile Picture")]
+        public IFormFile Image { get; set; }
+
+        [Required]
+        public int ImovelId { get; set; }
+        public Imovel Imovel { get; set; }
     }
 
 }
